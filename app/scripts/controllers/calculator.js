80618d668062d1a78e2012651d481921ecd934b6
Rito <rito@MBPs-MacBook-Pro.local>
2014-06-30 19:45:29 +0700

    start test function

--- app/scripts/controllers/calculator.js
+++ app/scripts/controllers/calculator.js
@@ -10,12 +10,36 @@
 angular.module('calculatorApp')
   .controller('CalculatorCtrl', function ($scope) {
   	var tempInput = [];
+  	$scope.inputTextArray = [];
+  	$scope.operatorArray = [];
     $scope.number = function (input) {
-    	tempInput.push(input);
-    	
-    	$scope.inputText =  parseInt(tempInput.join(''));
-    	
+    	if (typeof($scope.operator) != 'undefined' ) {
+   	    	$scope.operatorArray.push($scope.operator);
 
+    	}
+    	tempInput.push(input);
+    	$scope.inputText =  parseInt(tempInput.join(''));	
+    };
+
+    $scope.setOperator = function (operator) {
+    	$scope.operator = operator;
+    	if (tempInput.length == 0) {
+    		$scope.errorOperation = true;
+    		
+    	}
+    	if ($scope.inputText != "") {
+    		$scope.inputTextArray.push($scope.inputText);
+    		$scope.inputText = "";
+    		tempInput = [];
+    	}
+    }
+
+    $scope.calculate = function () {
+    	if ($scope.inputText != "") {
+    		$scope.inputTextArray.push($scope.inputText);
+    		$scope.inputText = "";
+    		tempInput = [];
+    	}
     };
 
   });
--- test/spec/controllers/calculator.js
+++ test/spec/controllers/calculator.js
@@ -36,7 +36,7 @@ describe('Controller: CalculatorCtrl', function () {
 
   });
 
-   it('ถ้ากด 1 แล้ว 2 แล้ว 3เรียก function number ค่า inputText ต้องได้ 12', function () {
+  it('ถ้ากด 1 แล้ว 2 แล้ว 3เรียก function number ค่า inputText ต้องได้ 12', function () {
     scope.number(1);
     scope.number(2);
     scope.number(3);
@@ -44,6 +44,109 @@ describe('Controller: CalculatorCtrl', function () {
     expect(scope.inputText).toBe(123);
 
   });
+
+  it('ถ้ากด + แล้ว operator จะเป็น +', function () {
+    scope.setOperator('+');
+    expect(scope.operator).toBe('+');
+  }); 
+
+  it('ถ้ากด - แล้ว operator จะเป็น -', function () {
+    scope.setOperator('-');
+    expect(scope.operator).toBe('-');
+  }); 
+
+  it('ถ้ากด * แล้ว operator จะเป็น *', function () {
+    scope.setOperator('*');
+    expect(scope.operator).toBe('*');
+  }); 
+
+  it('ถ้ากด / แล้ว operator จะเป็น /', function () {
+    scope.setOperator('/');
+    expect(scope.operator).toBe('/');
+  }); 
+
+  it('ถ้ากด +,- แล้ว operator จะเป็น -', function () {
+    scope.setOperator('+');
+    scope.setOperator('-');
+    expect(scope.operator).toBe('-');
+  }); 
+  it('ถ้ากด +,-,* แล้ว operator จะเป็น *', function () {
+    scope.setOperator('+');
+    scope.setOperator('-');
+    expect(scope.operator).toBe('-');
+  }); 
+  it('ถ้ากด +,-,* แล้ว operator จะเป็น *', function () {
+    scope.setOperator('+');
+    scope.setOperator('-');
+    scope.setOperator('*');
+    expect(scope.operator).toBe('*');
+  }); 
+
+  it('ถ้าหดเครื่องหมาย + ก่อนโดยไม่มีตัวเลข จะได้ค่า errorOperation = true', function () {
+    scope.setOperator('+');
+    expect(scope.errorOperation).toBe(true);
+  });
+
+  it('กด 123 แล้วตามด้วยเครื่องหมาย + จะได้ inputTextArray[0] = 123', function () {
+
+    scope.number(1);
+    scope.number(2);
+    scope.number(3);
+    scope.setOperator('+');
+    expect(scope.inputTextArray[0]).toBe(123);
+  });
+
+  it('เมื่อมีการเปลี่ยนจาก operation เป็นตัวเลข จะเก็บ operation ไว้ใน operationArray ', function () {
+
+    scope.number(1);
+    scope.number(2);
+    scope.number(3);
+    scope.setOperator('+');
+    scope.number(1);
+    expect(scope.operatorArray[0]).toBe('+');
+  });
+
+  it('กดปุ่ม enter เรียก function calulate ถ้ายังมีชุดตัวเลขสุดท้ายให้เอาไปใส่ใน  inputTextArray ท้ายสุด', function () {
+
+    scope.number(1);
+    scope.number(2);
+    scope.number(3);
+    scope.setOperator('+');
+    scope.number(1);
+    scope.calculate();
+    expect(scope.inputTextArray[1]).toBe(1);
+  });
+
+
+
+  it('ถ้ากด 123 และ +, -, *, + และ 456 และ + จะได้ค่า inputText[123, 456] และ operator = + ', function () {
+    // scope.number(1);
+    // scope.number(2);
+    // scope.number(3);
+
+
+    // scope.setOperator('-');
+    // scope.setOperator('*');
+    // scope.setOperator('+');
+    // expect(scope.operator).toBe('+');
+
+    // expect(scope.inputTextArray[0]).toBe(123);
+
+    // scope.number(4);
+    // scope.number(5);
+    // scope.number(6);
+
+    // scope.setOperator('+');
+    // expect(scope.inputTextArray[1]).toBe(456);
+  });
+
+
+
+
+
+
+
+
 
 
 
